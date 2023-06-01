db30d8e4426978a9df4f3588ebe5f33cee9b7201
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-06-01 15:54:36 +0200

    add lambda function

--- app/app_api.py
+++ app/app_api.py
@@ -1,17 +1,35 @@
-from fastapi import FastAPI
-from app import generate_branding_snippet, generate_keywords
+from fastapi import FastAPI, HTTPException
+from app import generate_branding_snippet, generate_keywords, validate_length
 
 app = FastAPI()
+
+MAX_INPUT_LENGHT = 12
 
 
 @app.get("/generate_snippet")
 async def generate_branding_snippet_route(prompt: str):
-    snippet = generate_branding_snippet(prompt).strip("\\")
-    return {"snippet": snippet}
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
 
 
 @app.get("/generate_keywords")
 async def generate_keywords_route(prompt: str):
+    validate_input_length(prompt)
     keywords = generate_keywords(prompt)
-    filtered_keywords = [keyword for keyword in keywords if keyword]
-    return {"keywords": filtered_keywords}
+    return {"snippet": None, "keywords": keywords}
+
+
+@app.get("/generate_snippet_keywords")
+async def generate_snippet_keywords(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGHT:
+        raise HTTPException(
+            status_code=400, 
+            detail=f"Input length is too long. Must be under {MAX_INPUT_LENGHT} characters."
+            )
+    pass
